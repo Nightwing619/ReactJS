456f578993aa83aa6251e1de37ba9420df33f891
Rakshit Raj <72436431+Nightwing619@users.noreply.github.com>
2022-04-28 21:41:07 +0530

    Add files via upload

---
+++ Project2/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Header from './Components/Header';
+import Meme from './Components/Meme';
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Meme />
+    </div>
+  );
+}
